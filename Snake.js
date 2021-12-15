1fa4458dd957fd136f07ed3f6179ef32f526dfed
marko <obrenicmarko3@gmail.com>
2021-12-15 23:03:02 +0100

    basic logic

--- Snake.js
+++ Snake.js
@@ -1,52 +1,106 @@
 
 class Snake {
     constructor() {
-        this.snakeDots = [
-            this.getDot(20,20, 'right'),
-            this.getDot(19,20, 'right'),
-            this.getDot(18,20, 'right'),
-        ];
+        this.snakeDots = [];
+        this.mouse = null;
+        this._score = null;
+        // ground 40 x 40
+        this.ground = document.getElementById('ground');
+
+        this.startNewGame();
     }
 
-    // addDots(initX, initY, direction, length = 1) {
-    //     this.snakeDots.push(this.getDot(initX, initY, direction))
-    //
-    //     for (let i = 1; i < length; i++) {
-    //         switch (direction) {
-    //             case 'left':
-    //                 initX += 1;
-    //                 break;
-    //             case 'up':
-    //                 initY +=1;
-    //                 break;
-    //             case 'right':
-    //                 initX -= 1;
-    //                 break;
-    //             case 'down':
-    //                 initY -= 1;
-    //                 break;
-    //         }
-    //
-    //         this.snakeDots.push(this.getDot(initX, initY, direction));
-    //     }
-    // }
+    startNewGame() {
+        this.snakeDots = [
+            this.getDot(20,20, 'right', true),
+            this.getDot(19,20, 'right'),
+            this.getDot(18,20, 'right'),
+            this.getDot(17,20, 'right'),
+            this.getDot(16,20, 'right'),
+            this.getDot(15,20, 'right'),
+            this.getDot(14,20, 'right'),
+            this.getDot(13,20, 'right'),
+        ];
+        this.mouse = this.getMouse();
+        this.score = 0;
 
-    getDot(x, y , direction) {
+        this.ground.innerHTML = "";
+        this.ground.appendChild(this.mouse.template);
+        for(let snakeDot of this.snakeDots) {
+            this.ground.appendChild(snakeDot.template);
+        }
+    }
+
+    get snakeHead() {
+        return this.snakeDots[0];
+    }
+
+    set score(value) {
+        this._score = value;
+        document.getElementById('score').innerHTML = value;
+    }
+
+    get score() {
+        return this._score;
+    }
+
+
+    changeMousePosition() {
+        this.mouse.position = {...this.getMouseRandomPosition()};
+
+        this.mouse.template.style.top = (this.mouse.position.y * 15) + 'px';
+        this.mouse.template.style.left = (this.mouse.position.x * 15) + 'px';
+    }
+
+
+    getMouse() {
+        const randomPosition = this.getMouseRandomPosition();
+
+        const mouseTemplate = document.createElement('div');
+
+        mouseTemplate.classList.add('dot');
+        mouseTemplate.style.top = (randomPosition.y * 15) + 'px';
+        mouseTemplate.style.left = (randomPosition.x * 15) + 'px';
+
+        return {
+            position: randomPosition,
+            template: mouseTemplate
+        }
+    }
+
+    getDot(x, y , direction, isHead = false) {
         return {
             position: {x: x, y: y},
             direction: direction,
             template: this.getDotTemplate(x, y),
-            stack: []
+            stack: [],
+            isHead: isHead
         }
     }
 
     getDotTemplate(x, y) {
         const snakeDotTemplate = document.createElement('div');
-        snakeDotTemplate.classList.add('snake-dot');
+        snakeDotTemplate.classList.add('dot');
         snakeDotTemplate.style.top = (y * 15) + 'px';
         snakeDotTemplate.style.left = (x * 15) + 'px';
 
         return snakeDotTemplate;
+    }
+
+    getMouseRandomPosition() {
+        let exist = false;
+        let randomPosition = null;
+
+        do {
+            randomPosition = this.randomPosition();
+
+            exist = this.snakeDots.some((dot) => {
+                return JSON.stringify(dot.position) === JSON.stringify(randomPosition);
+            });
+
+        } while(exist);
+
+        return randomPosition;
     }
 
     move() {
@@ -56,39 +110,79 @@ class Snake {
                 dot.stack.shift();
             }
 
-            switch (dot.direction) {
-                case 'left':
-                    if(dot.position.x === 0) {
-                        dot.position.x = 39;
-                    } else {
-                        dot.position.x -= 1;
-                    }
-                    break;
-                case 'up':
-                    if(dot.position.y === 0) {
-                        dot.position.y = 39;
-                    } else {
-                        dot.position.y -= 1;
-                    }
-                    break;
-                case 'right':
-                    if(dot.position.x === 39) {
-                        dot.position.x = 0;
-                    } else {
-                        dot.position.x += 1;
-                    }
-                    break;
-                case 'down':
-                    if(dot.position.y === 39) {
-                        dot.position.y = 0;
-                    } else {
-                        dot.position.y += 1;
-                    }
-                    break;
+            this.changeDotPosition(dot);
+
+            // check is game over if current dot is head
+            if(dot.isHead && this.isGameOver()) {
+                this.handleGameOver();
             }
 
             dot.template.style.top = dot.position.y * 15 + 'px';
             dot.template.style.left = dot.position.x * 15 + 'px';
+
+            if(dot.isHead && this.isMouseEaten()) {
+                this.changeMousePosition();
+                this.sizeUpSnake();
+                this.score += 1;
+            }
+        }
+    }
+
+    sizeUpSnake() {
+        let latestDot = this.snakeDots[this.snakeDots.length - 1];
+
+        let newDot = this.getDot(latestDot.position.x, latestDot.position.y, latestDot.direction);
+        newDot.stack = [...latestDot.stack];
+
+        switch (latestDot.direction) {
+            case 'left':
+                newDot.position.x += 1;
+                break;
+            case 'up':
+                newDot.position.y +=1;
+                break;
+            case 'right':
+                newDot.position.x -= 1;
+                break;
+            case 'down':
+                newDot.position.y -= 1;
+                break;
+        }
+
+        this.snakeDots.push(newDot);
+        this.ground.appendChild(newDot.template);
+    }
+
+    changeDotPosition(dot) {
+        switch (dot.direction) {
+            case 'left':
+                if(dot.position.x === 0) {
+                    dot.position.x = 39;
+                } else {
+                    dot.position.x -= 1;
+                }
+                break;
+            case 'up':
+                if(dot.position.y === 0) {
+                    dot.position.y = 39;
+                } else {
+                    dot.position.y -= 1;
+                }
+                break;
+            case 'right':
+                if(dot.position.x === 39) {
+                    dot.position.x = 0;
+                } else {
+                    dot.position.x += 1;
+                }
+                break;
+            case 'down':
+                if(dot.position.y === 39) {
+                    dot.position.y = 0;
+                } else {
+                    dot.position.y += 1;
+                }
+                break;
         }
     }
 
@@ -103,12 +197,52 @@ class Snake {
     }
 
     changeDirection(newDirection) {
+        if(
+            (['left', 'right'].includes(this.snakeHead.direction) && ['left', 'right'].includes(newDirection))
+            || (['up', 'down'].includes(this.snakeHead.direction) && ['up', 'down'].includes(newDirection))
+        ) {
+            return;
+        }
+
         for(let dot of this.snakeDots) {
+            //check is duplicate
+            const exists = dot.stack.find((item) => {
+                return JSON.stringify(item.onPosition) === JSON.stringify(this.snakeHead.position);
+            })
+
+            if(exists) return;
+
             dot.stack.push({
                 newDirection: newDirection,
-                onPosition: {...this.snakeDots[0].position}
+                onPosition: {...this.snakeHead.position}
             })
         }
     }
 
+    randomPosition() {
+        return {
+            x: Math.floor(Math.random() * 40),
+            y: Math.floor(Math.random() * 40)
+        }
+    }
+
+    isGameOver() {
+        for(let i = 1; i < this.snakeDots.length; i++) {
+            if(JSON.stringify(this.snakeHead.position) === JSON.stringify(this.snakeDots[i].position)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    isMouseEaten() {
+        return JSON.stringify(this.snakeHead.position) === JSON.stringify(this.mouse.position);
+    }
+
+    handleGameOver() {
+        alert("Game over!\nClick ok and start new game.");
+
+        this.startNewGame();
+    }
 }
--- app.js
+++ app.js
@@ -1,38 +1,23 @@
 const snake = new Snake();
 
-// ground 40 x 40
-const ground = document.getElementById('ground');
-
-
-ground.innerHTML = '';
-
-for(let snakeDot of snake.snakeDots) {
-    ground.appendChild(snakeDot.template);
+const controls = {
+    "ArrowUp": "up",
+    "ArrowRight": "right",
+    "ArrowDown": "down",
+    "ArrowLeft": "left",
 }
+
 
 
 setInterval(() => {
     snake.move()
-}, 500)
+}, 100)
 
 document.addEventListener('keydown', (event) => {
     if(event.code === 'Space') {
         snake.move();
     }
 
-    switch (event.code) {
-        case "ArrowUp":
-            snake.changeDirection("up");
-            break;
-        case "ArrowRight":
-            snake.changeDirection("right");
-            break;
-        case "ArrowDown":
-            snake.changeDirection("down");
-            break;
-        case "ArrowLeft":
-            snake.changeDirection("left");
-            break;
-    }
+    snake.changeDirection(controls[event.code]);
 })
 
